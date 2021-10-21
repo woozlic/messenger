19fea11c25ce58f2948372e88abd21000e4ae652
woozlic <sampaccs@mail.ru>
2021-10-21 14:24:23 +0500

    add chat rooms for multiple users

--- messenger/settings.py
+++ messenger/settings.py
@@ -29,6 +29,15 @@ ALLOWED_HOSTS = []
 
 ASGI_APPLICATION = 'messenger.asgi.application'
 
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
+
 # Application definition
 
 INSTALLED_APPS = [
