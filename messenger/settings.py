b82174406cef0850e7b9995c5370715338c27e33
woozlic <sampaccs@mail.ru>
2021-10-21 15:10:14 +0500

    add test chat in same room and diffirent room

--- messenger/settings.py
+++ messenger/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os.path
 from pathlib import Path
 from secrets import SECRET_KEY
 
@@ -91,6 +91,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'TEST': {
+            'NAME': os.path.join(BASE_DIR, 'db_test.sqlite3')
+        }
     }
 }
 
