153183f7bbeffc62a927208c82807ca93eb3ccc9
woozlic <sampaccs@mail.ru>
2021-10-21 14:02:09 +0500

    set up channels and asgi

--- messenger/settings.py
+++ messenger/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from secrets import SECRET_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,13 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wupf0^vrqf^d7=c2n9_-1ctbc9wa9!dfkf4&(=1b-tu$g!_l)g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
 
+ASGI_APPLICATION = 'messenger.asgi.application'
 
 # Application definition
 
@@ -37,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'channels',
+
+    'chat',
 ]
 
 MIDDLEWARE = [
