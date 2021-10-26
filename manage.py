52fda309aa8e2aadf4827eba4f228a341d4e5817
woozlic <sampaccs@mail.ru>
2021-10-26 17:24:13 +0500

    add run docker from manage.py and requirements.txt

--- manage.py
+++ manage.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
 import os
+import subprocess
 import sys
 
 
@@ -9,6 +10,7 @@ def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'messenger.settings')
     try:
         from django.core.management import execute_from_command_line
+        subprocess.call(['docker', 'run', '-p', '6379:6379', '-d', 'redis:5'])
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
