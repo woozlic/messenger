cefdb980afd426bf83d7e75886ac238a6f00d8fc
woozlic <sampaccs@mail.ru>
2021-12-14 12:57:56 +0300

    add message styles

--- messenger/urls.py
+++ messenger/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
+from chat import views
 
 urlpatterns = [
     path('chat/', include('chat.urls')),
+    path('', views.index),
     path('admin/', admin.site.urls),
 ]
