8b21e596e2430ec9992689c162d7dcb7eac1046a
Dmitry Demin <dmitry.demin@megafon.ru>
2022-06-17 16:06:54 +0500

    automatically creates 2 users

--- README.md
+++ README.md
@@ -22,6 +22,14 @@ SECRET_KEY=there_must_be_random_string
 docker-compose up --build
 ```
 
+### IMPORTANT!
+
+**Docker will automatically create 2 superusers with passwords for testing**
+
+```test_1: admin```
+
+```test_2: admin```
+
 ## Use
 
 Visit [Online Messenger](http://127.0.0.1:7777) in your favorite browser
---
+++ src/chat/management/commands/initadmin.py
@@ -0,0 +1,19 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+
+ADMINS = ('test_1', 'test_2', 'test_3')
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        if User.objects.count() == 0:
+            for username in ADMINS:
+                password = 'admin'
+                self.stdout.write('Creating account for %s' % username)
+                admin = User.objects.create_superuser(username=username, password=password)
+                admin.is_active = True
+                admin.is_admin = True
+                admin.save()
+        else:
+            self.stdout.write('Admin accounts can only be initialized if no Accounts exist')
