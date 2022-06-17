b8156efde891ccf92ad0a1146f00daa930997488
Dmitry Demin <dmitry.demin@megafon.ru>
2022-06-17 15:15:36 +0500

    fix staticfiles

--- src/chat/views.py
+++ src/chat/views.py
@@ -2,15 +2,15 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'chat/templates/chat/index.html')
+    return render(request, 'chat/index.html')
 
 
 def exit(request):
-    return render(request, 'chat/templates/chat/exit.html')
+    return render(request, 'chat/exit.html')
 
 
 def room(request, room_name):
-    return render(request, 'chat/templates/chat/room.html', {
+    return render(request, 'chat/room.html', {
         'room_name': room_name,
         'user': request.user
     })
--- src/messenger/settings.py
+++ src/messenger/settings.py
@@ -140,16 +140,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'src/static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / "src/static",
     '/var/www/static/',
+    os.path.join(BASE_DIR, 'static')
 ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATIC_URL = '/static/'
 STATIC_ROOT = '/var/www/chatiq.com/static/'
